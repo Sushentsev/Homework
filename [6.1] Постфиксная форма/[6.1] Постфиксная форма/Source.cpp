575df08707a1f3c4e96dc9a5107162ff17172b32
Sushentsev Denis <sushentsevds@gmail.com>
2016-11-03 00:50:58 +0300

    Solving postfix expression using stack

---
+++ [6.1] Постфиксная форма/[6.1] Постфиксная форма/Source.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include "Stack.h"
+
+int result(Stack *stack, std::string s)
+{
+	for (int i = 0; i < s.length(); ++i)
+	{
+		int value1 = 0;
+		int value2 = 0;
+		char c[1] = { s[i] };
+		if ((c[0] >= '0') && (c[0] <= '9'))
+		{
+			push(stack, atoi(c));
+		}
+		switch (c[0])
+		{
+		case '+':
+			value1 = pop(stack);
+			value2 = pop(stack);
+			push(stack, value1 + value2);
+			break;
+		case '-':
+			value1 = pop(stack);
+			value2 = pop(stack);
+			push(stack, value2 - value1);
+			break;
+		case '*':
+			value1 = pop(stack);
+			value2 = pop(stack);
+			push(stack, value1 * value2);
+			break;
+		case '/':
+			value1 = pop(stack);
+			value2 = pop(stack);
+			push(stack, value2 / value1);
+			break;
+		default:
+			break;
+		}
+	}
+	return pop(stack);
+}
+
+bool test()
+{
+	Stack *stack = createStack();
+	std::string s = "9 6 - 1 2 + *";
+	return result(stack, s) == 9;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	Stack *stack = createStack();
+	std::string s = "";
+	std::cout << "Test: " << test() << std::endl;
+	std::cout << "¬ведите выражение в постфиксной записи:" << std::endl;
+	std::getline(std::cin, s);
+	std::cout << "«начение примера равно: " << result(stack, s) << std::endl;
+	deleteStack(stack);
+	delete stack;
+	stack = nullptr;
+}
