a14e45a04a2b14b770c879267de27baebf104139
Sushentsev Denis <sushentsevds@gmail.com>
2016-11-03 18:53:13 +0300

    Added strings, new function

--- [6.3] Сортировочная станция/[6.3] Сортировочная станция/Source.cpp
+++ [6.3] Сортировочная станция/[6.3] Сортировочная станция/Source.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "Stack.h"
+#include <string>
 
-
+int postfixForm(const std::string &infixform)
+{
+	std::string postfix = "";
+}
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 	Stack *stack = createStack();
+	std::string infixForm = "";
+	std::cout << "¬ведите выражение в инфиксной форме:" << std::endl;
+	std::getline(std::cin, infixForm);
+
+	delete stack;
+	stack = nullptr;
 }
