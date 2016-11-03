bbce43afad03fc3f846cd3cb744eebec3660d794
Sushentsev Denis <sushentsevds@gmail.com>
2016-11-03 19:24:35 +0300

    Added direction, some small things

--- [6.3] Сортировочная станция/[6.3] Сортировочная станция/Source.cpp
+++ [6.3] Сортировочная станция/[6.3] Сортировочная станция/Source.cpp
@@ -2,22 +2,43 @@
 #include "Stack.h"
 #include <string>
 
-void fromInfixFormToPostFix(const std::string &infixForm, std::string &postfixForm)
+int direction(const std::string element)
 {
+	int status = -1;
 
+	return status;
+}
+
+int fromInfixFormToPostFix(Stack *stack, const std::string &infixForm, std::string &postfixForm)
+{
+	int status = -1;
+	for (int i = 0; i < infixForm.length(); ++i)
+	{
+
+	}
+	return status;
 }
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 	Stack *stack = createStack();
+	int status = 0;
 	std::string infixForm = "";
 	std::string postfixForm = "";
 	std::cout << "Введите выражение в инфиксной форме:" << std::endl;
 	std::getline(std::cin, infixForm);
 	infixForm = infixForm + "|";
-	fromInfixFormToPostFix(infixForm, postfixForm);
-	std::cout << "Постфиксная форма выражения: " << postfixForm << std::endl;
+	status = fromInfixFormToPostFix(stack, infixForm, postfixForm);
+	if (status == 4)
+	{
+		std::cout << "Постфиксная форма выражения: " << postfixForm << std::endl;
+	}
+	else
+	{
+		std::cout << "Произошла ошибка при переводе" << std::endl;
+	}
+	deleteStack(stack);
 	delete stack;
 	stack = nullptr;
 }
