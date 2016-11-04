45a9025a3d129af008dc531d3f790a35e35203bc
Sushentsev Denis <sushentsevds@gmail.com>
2016-11-04 11:39:15 +0300

    Added structures TreeElement and Tree

---
+++ [7.1] АТД Множество/[7.1] АТД Множество/tree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "tree.h"
+
+struct TreeElement
+{
+	int value;
+	TreeElement *parent;
+	TreeElement *leftChild;
+	TreeElement *rightChild;
+};
+
+struct Tree
+{
+	TreeElement *rootl
+};
