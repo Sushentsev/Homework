e0e7bdf27643804965d7dd6379bf147297e8dbe4
Sushentsev Denis <sushentsevds@gmail.com>
2016-12-07 18:30:11 +0300

    Test not finished

--- [5.1] Работа со списком/[5.1] Работа со списком/Source.cpp
+++ [5.1] Работа со списком/[5.1] Работа со списком/Source.cpp
@@ -1,172 +1,76 @@
 #include <iostream>
+#include "list.h"
 
-struct ListElement
+void printInformation()
 {
-	int value;
-	ListElement *next;
-};
-
-struct List
-{
-	ListElement *head;
-};
-
-List* createList()
-{
-	List *list = new List;
-	list->head = nullptr;
-	return list;
+	std::cout << std::endl;
+	std::cout << "¬ыберете операцию:" << std::endl;
+	std::cout << "0 Ц выйти" << std::endl;
+	std::cout << "1 Ц добавить значение в сортированный список" << std::endl;
+	std::cout << "2 Ц удалить значение из списка" << std::endl;
+	std::cout << "3 Ц распечатать список" << std::endl;
+	std::cout << std::endl;
 }
 
-ListElement* createListElement(int value, ListElement *next)
+void addElement(List *list)
 {
-	ListElement *newElement = new ListElement;
-	newElement->value = value;
-	newElement->next = next;
-	return newElement;
-}
-
-bool isEmpty(List *list)
-{
-	return list->head == nullptr;
-}
-
-void addElement(List *list, int value)
-{
-	if (isEmpty(list))
-	{
-		list->head = createListElement(value, nullptr);
-		std::cout << "—оздан первый элемент" << std::endl;
-		return;
-	}
-
-	ListElement *temp = list->head;
-	if (temp->value > value)
-	{
-		temp = createListElement(value, temp);
-		list->head = temp;
-		std::cout << "Ёлемент успешно добавлен!" << std::endl;
-		return;
-	}
-	while (temp->next != nullptr && temp->next->value < value)
-	{
-		temp = temp->next;
-	}
-	temp->next = createListElement(value, temp->next);
+	int value = 0;
+	std::cout << "¬ведите значение добавл€емого элемента:" << std::endl;
+	std::cin >> value;
+	addElement(list, value);
 	std::cout << "Ёлемент успешно добавлен!" << std::endl;
 }
 
-void removeElement(List *list, int value)
+void removeElement(List *list)
 {
-	if (isEmpty(list))
-	{
-		std::cout << "—писок пуст!" << std::endl;
-		return;
-	}
-	ListElement *temp = list->head;
-	if (temp->next == nullptr && temp->value == value)
-	{
-		delete temp;
-		temp = nullptr;
-		list->head = nullptr;
-		std::cout << "Ёлемент успешно удален!" << std::endl;
-		return;
-	}
-	if (temp->value == value)
-	{
-		ListElement *toDelete = temp;
-		list->head = temp->next;
-		delete toDelete;
-		toDelete = nullptr;
-		std::cout << "Ёлемент успешно удален!" << std::endl;
-		return;
-	}
-	while (temp->next != nullptr && temp->next->value != value)
-	{
-		temp = temp->next;
-	}
-	if (temp->next == nullptr)
-	{
-		std::cout << "“акого элемента нет" << std::endl;
-		return;
-	}
-	ListElement *toDelete = temp->next;
-	temp->next = temp->next->next;
-	delete toDelete;
-	toDelete = nullptr;
-	std::cout << "Ёлемент успешно удален!" << std::endl;
+	int value = 0;
+	std::cout << "¬ведите значение удал€емого элемента:" << std::endl;
+	std::cin >> value;
+	if (removeElement(list, value))
+		std::cout << "”даление элемента прошло успешно!" << std::endl;
+	else
+		std::cout << "ќшибка при удалении!" << std::endl;
 }
 
-void printList(List *list)
+bool test(List *list)
 {
-	if (isEmpty(list))
-	{
-		std::cout << "—писок пуст!" << std::endl;
-		return;
-	}
-	ListElement *temp = list->head;
-	while (temp->next != nullptr)
-	{
-		std::cout << temp->value << " ";
-		temp = temp->next;
-	}
-	std::cout << temp->value << std::endl;
-	std::cout << "¬се значени€ распечатаны!" << std::endl;
-}
-
-void deleteList(List *list)
-{
-	if (isEmpty(list))
-	{
-		return;
-	}
-	while (list->head != nullptr)
-	{
-		ListElement *toDelete = list->head;
-		list->head = list->head->next;
-		delete toDelete;
-		toDelete = nullptr;
-	}
-	delete list;
-	list = nullptr;
+	bool result = true;
+	addElement(list, 0);
+	addElement(list, -1);
+	addElement(list, 1);
+	return result;
 }
 
 void main()
 {
+
 	setlocale(LC_ALL, "Russian");
 	List *list = createList();
+	std::cout << test(list) << std::endl;
+	deleteList(list)
+
+	List *list = createList();
 	short selection = -1;
+
 	while (selection != 0)
 	{
-		int value = 0;
-		std::cout << "¬ыберете операцию:" << std::endl;
-		std::cout << "0 Ц выйти" << std::endl;
-		std::cout << "1 Ц добавить значение в сортированный список" << std::endl;
-		std::cout << "2 Ц удалить значение из списка" << std::endl;
-		std::cout << "3 Ц распечатать список" << std::endl;
-		std::cout << std::endl;
+		printInformation();
 		std::cin >> selection;
 		switch (selection)
 		{
 		case 1:
-			std::cout << "¬ведите значение добавл€емого элемента:" << std::endl;
-			std::cin >> value;
-			addElement(list, value);
-			std::cout << std::endl;
+			addElement(list);
 			break;
 		case 2:
-			std::cout << "¬ведите значение удал€емого элемента:" << std::endl;
-			std::cin >> value;
-			removeElement(list, value);
-			std::cout << std::endl;
+			removeElement(list);
 			break;
 		case 3:
 			printList(list);
-			std::cout << std::endl;
 			break;
 		default:
 			break;
 		}
 	}
+
 	deleteList(list);
 }
