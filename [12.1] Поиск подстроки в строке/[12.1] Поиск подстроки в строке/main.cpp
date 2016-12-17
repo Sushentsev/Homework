17a173406c98ff669525883e0446db38c1a3568f
Sushentsev Denis <sushentsevds@gmail.com>
2016-12-17 17:32:56 +0300

    Added prefix function

--- [12.1] Поиск подстроки в строке/[12.1] Поиск подстроки в строке/main.cpp
+++ [12.1] Поиск подстроки в строке/[12.1] Поиск подстроки в строке/main.cpp
@@ -18,24 +18,37 @@ void loadFromFile(string &haystack)
 	}
 }
 
-
-
-void test1()
+void prefixFunction(const string &concatenatedString, int *prefixArray)
 {
-	Stack *stack = createStack();
-	push(stack, 'a', 1);
-	push(stack, 'b', 2);
-	push(stack, 'd', 3);
-	cout << "Is empty: " << isEmpty(stack) << endl;
-	cout << "Is contained (a): " << isContained(stack, 'a') << endl;
-	cout << "Is contained (c): " << isContained(stack, 'c') << endl;
-	cout << "Value (b): " << returnValue(stack, 'b') << endl;
-	cout << "Value (b): " << returnValue(stack, 'b') << endl;
-	printStack(stack);
-	pop(stack);
-	printStack(stack);
-	deleteStack(stack);
-	cout << endl << endl;
+	const int stringLength = concatenatedString.length();
+	prefixArray[0] = 0;
+	for (int i = 1; i < stringLength; ++i)
+	{
+		int position = prefixArray[i - 1];
+		while ((position > 0) && (concatenatedString[i] != concatenatedString[position]))
+		{
+			position = prefixArray[position - 1];
+		}
+		if (concatenatedString[i] == concatenatedString[position])
+		{
+			++position;
+		}
+		prefixArray[i] = position;
+	}
+}
+
+bool test1()
+{
+	bool flag = 1;
+	const int stringLength = 7;
+	string concatenatedString = "aabaaab";
+	int *prefixArray = new int[stringLength]{};
+	prefixFunction(concatenatedString, prefixArray);
+	flag = (prefixArray[0] == 0) && (prefixArray[1] == 1) && (prefixArray[2] == 0) && (prefixArray[3] == 1) &&
+		(prefixArray[4] == 2) && (prefixArray[5] == 2) && (prefixArray[6] == 3);
+	delete[] prefixArray;
+	prefixArray = nullptr;
+	return flag;
 }
 
 void main()
@@ -43,7 +56,19 @@ void main()
 	setlocale(LC_ALL, "Russian");
 	string haystack = "";
 	string needle = "";
-	cout << "¬ведите образец:" << endl;
+	string concatenatedString = "";
+	cout << "Test 1 (test prefix function): " << test1() << endl;
+	cout << "Test 2 (test getting position): " << test2() << endl;
+	cout << "Test 3 (test getting position): " << test3() << endl;
+
+ 	cout << "¬ведите образец:" << endl;
 	getline(cin, needle);
 	loadFromFile(haystack);
+
+	concatenatedString = needle + "#" + haystack;
+	int *prefixArray = new int[concatenatedString.length()]{};
+	prefixFunction(concatenatedString, prefixArray);
+
+	delete[] prefixArray;
+	prefixArray = nullptr;
 }
