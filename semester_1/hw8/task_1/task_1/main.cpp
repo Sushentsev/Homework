0af220d4682eea98a77c6988859150c7d851b323
Sushentsev Denis <sushentsevds@gmail.com>
2016-12-25 02:08:50 +0300

    Problems with parents not finished

--- semester_1/hw8/task_1/task_1/main.cpp
+++ semester_1/hw8/task_1/task_1/main.cpp
@@ -4,7 +4,28 @@
 
 using namespace std;
 
+//void printInformation();
+
+bool test()
+{
+	auto tree = plantTree();
+
+	string key1 = "1";
+	string key2 = "2";
+	string value1 = "value1";
+	string value2 = "value2";
+
+	addNode(tree, key1, value1);
+	addNode(tree, key2, value2);
+	removeNode(tree, key1);
+	const bool ok = ((isContained(tree, key1) == false) && (getValue(tree, key2) == "value2"));
+
+	removeTree(tree);
+
+	return ok;
+}
+
 void main()
 {
-
+	cout << test();
 }
--- semester_1/hw8/task_1/task_1/splayTree.cpp
+++ semester_1/hw8/task_1/task_1/splayTree.cpp
@@ -35,62 +35,157 @@ TreeNode* createTreeNode(const string &key, const string &value, TreeNode *leftC
 	return newNode;
 }
 
-void zig(TreeNode *&node)
+TreeNode* getParent(TreeNode *node)
 {
-
+	if (node->parent == nullptr)
+	{
+		return nullptr;
+	}
+	return node->parent;
 }
 
-void zigZag(TreeNode *&node)
+bool isEmpty(Tree *tree)
 {
-
+	return (tree->root == nullptr);
 }
 
-void zigZig(TreeNode *&node)
+void turnRight(TreeNode *&child, TreeNode *&parent)
 {
-
+	parent->leftChild = child->rightChild;
+	child->parent = parent->parent;
+	parent->parent = child;
+	child->rightChild = parent;
 }
 
-void splay(Tree *tree)
+void turnLeft(TreeNode *&child, TreeNode *&parent)
 {
-
+	parent->rightChild = child->leftChild;
+	child->parent = parent->parent;
+	parent->parent = child;
+	child->leftChild = parent;
 }
 
-string getNodeValue(TreeNode *node, const string &key)
+void zig(TreeNode *&child, TreeNode *&parent)
+{
+	if (parent->leftChild == child)
+	{
+		turnRight(child, parent);
+	}
+	else if (parent->rightChild == child)
+	{
+		turnLeft(child, parent);
+	}
+}
+
+void zigZag(TreeNode *&child, TreeNode *&parent)
+{
+	if (parent->leftChild == child)
+	{
+		turnRight(child, parent);
+		turnLeft(child, child->parent);
+	}
+	else if (parent->rightChild == child)
+	{
+		turnLeft(child, parent);
+		turnRight(child, child->parent);
+	}
+}
+
+void zigZig(TreeNode *&child, TreeNode *&parent)
+{
+	if (parent->leftChild == child)
+	{
+		turnRight(parent, parent->parent);
+		turnRight(child, parent);
+	}
+	else if (parent->rightChild == child)
+	{
+		turnLeft(parent, parent->parent);
+		turnLeft(child, parent);
+	}
+}
+
+bool isRoot(TreeNode *node)
+{
+	return (node->parent == nullptr);
+}
+
+bool isBothLeftChildren(TreeNode *child, TreeNode *parent)
+{
+	return (parent->leftChild == child && parent->parent->leftChild == parent);
+}
+
+bool isBothRightChildren(TreeNode *child, TreeNode *parent)
+{
+	return (parent->rightChild == child && parent->parent->rightChild == parent);
+}
+
+void splay(TreeNode *&node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+
+	while (!isRoot(node))
+	{
+		if (isRoot(node->parent))
+		{
+			zig(node, node->parent);
+		}
+		else if (isBothLeftChildren(node, node->parent) || isBothRightChildren(node, node->parent))
+		{
+			zigZig(node, node->parent);
+		}
+		else
+		{
+			zigZag(node, node->parent);
+		}
+	}
+}
+
+string getValue(TreeNode *&node, const string &key)
 {
 	if (node->value->key == key)
 	{
+		splay(node);
 		return node->value->value;
 	}
 	else if (node->leftChild == nullptr && node->rightChild == nullptr)
 	{
-		//will be splaying
+		splay(node);
 		return "";
 	}
 	else if (key < node->value->key)
 	{
-		return getNodeValue(node->leftChild, key);
+		return getValue(node->leftChild, key);
 	}
 	else if (key > node->value->key)
 	{
-		return getNodeValue(node->rightChild, key);
+		return getValue(node->rightChild, key);
 	}
 }
 
 string getValue(Tree *tree, const string &key)
 {
-	return getNodeValue(tree->root, key);
+	if (isEmpty(tree))
+	{
+		return "";
+	}
+	return getValue(tree->root, key);
 }
 
-bool isContained(TreeNode *node, const string &key)
+bool isContained(TreeNode *&node, const string &key)
 {
 	if (node->value->key == key)
 	{
+		splay(node);
 		return true;
 	}
 	else if (node->leftChild == nullptr && node->rightChild == nullptr)
 	{
-		//will be splaying
-		return "";
+		splay(node);
+		return false;
 	}
 	else if (key < node->value->key)
 	{
@@ -104,17 +199,146 @@ bool isContained(TreeNode *node, const string &key)
 
 bool isContained(Tree *tree, const string &key)
 {
+	if (isEmpty(tree))
+	{
+		return false;
+	}
 	return isContained(tree->root, key);
 }
 
-void addValue(Tree *tree, const string &key, const string &value)
+void addNode(TreeNode *&node, const string &key, const string &value)
 {
 
+	auto parent = node->parent;
+	if (node == nullptr)
+	{
+		node = createTreeNode(key, value, nullptr, nullptr, parent);
+		splay(node);
+		return;
+	}
+	else if (node->value->key == key)
+	{
+		node->value->value = value;
+		splay(node);
+		return;
+	}
+	else if (key < node->value->key)
+	{
+		return addNode(node->leftChild, key, value);
+	}
+	else if (key > node->value->key)
+	{
+		return addNode(node->rightChild, key, value);
+	}
 }
 
-void removeValue(Tree *tree, const string &key)
+void addNode(Tree *tree, const string &key, const string &value)
 {
+	if (isEmpty(tree))
+	{
+		tree->root = createTreeNode(key, value, nullptr, nullptr, nullptr);
+		return;
+	}
 
+	addNode(tree->root, key, value);
+}
+
+TreeNode *findMinNode(TreeNode *node)
+{
+	auto minNode = node->rightChild;
+	while (minNode->leftChild != nullptr)
+	{
+		minNode = minNode->leftChild;
+	}
+	return minNode;
+}
+
+bool hasBothChildren(TreeNode *node)
+{
+	return (node->leftChild != nullptr && node->rightChild != nullptr);
+}
+
+bool hasNotChildren(TreeNode *node)
+{
+	return (node->leftChild == nullptr && node->rightChild == nullptr);
+}
+
+bool hasOnlyLeftChild(TreeNode *node)
+{
+	return (node->leftChild != nullptr && node->rightChild == nullptr);
+}
+
+bool hasOnlyRightChild(TreeNode *node)
+{
+	return (node->leftChild == nullptr && node->rightChild != nullptr);
+}
+
+void removeNode(TreeNode *&node, const string &key)
+{
+	auto parent = node->parent;
+	if (node == nullptr)
+	{
+		splay(parent);
+		return;
+	}
+	else if (node->value->key == key)
+	{
+		if (hasNotChildren(node))
+		{
+			delete node;
+			node = nullptr;
+			splay(parent);
+		}
+		else if (hasBothChildren(node))
+		{
+			auto minNode = findMinNode(node);
+			const string temp1 = minNode->value->key;
+			const string temp2 = minNode->value->value;
+			removeNode(node, key);
+			node->value->key = temp1;
+			node->value->value = temp2;
+			splay(parent);
+		}
+		else if (hasOnlyLeftChild(node))
+		{
+			auto toDelete = node;
+			node = node->leftChild;
+			delete toDelete;
+			splay(parent);
+		}
+		else if (hasOnlyRightChild(node))
+		{
+			auto toDelete = node;
+			node = node->rightChild;
+			delete toDelete;
+			splay(parent);
+		}
+		return;
+	}
+	else if (key > node->value->key)
+	{
+		return removeNode(node->rightChild, key);
+	}
+	else if (key < node->value->key)
+	{
+		return removeNode(node->leftChild, key);
+	}
+}
+
+void removeNode(Tree *tree, const string &key)
+{
+	if (isEmpty(tree))
+	{
+		return;
+	}
+
+	return removeNode(tree->root, key);
+}
+
+void removeValue(TreeNode *&node)
+{
+	delete node->value;
+	node->value = nullptr;
 }
 
 void removeTree(TreeNode *&node)
@@ -131,12 +355,18 @@ void removeTree(TreeNode *&node)
 	{
 		removeTree(node->rightChild);
 	}
+	removeValue(node);
 	delete node;
 	node = nullptr;
 }
 
 void removeTree(Tree *&tree)
 {
+	if (isEmpty(tree))
+	{
+		return;
+	}
+
 	removeTree(tree->root);
 	delete tree;
 	tree = nullptr;
