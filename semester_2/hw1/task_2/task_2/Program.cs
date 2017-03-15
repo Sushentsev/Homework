bbee24957b1287363ef9014b84ddb0e91fdf4279
Sushentsev Denis <sushentsevds@gmail.com>
2017-03-15 23:05:41 +0300

    Finished without tests

---
+++ semester_2/hw1/task_2/task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        /// <summary>
+        /// Calculating the n-th Fibonacci number
+        /// </summary>
+        /// <returns>Fibonacci number</returns>
+        private static int Fibonacci(int n)
+        {
+            if (n < 1)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The {0}th number is {1}", n, Fibonacci(n));
+        }
+    }
+}
