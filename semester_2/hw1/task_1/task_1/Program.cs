821dc7bc08de5027f1d29fcee597b17e59409cd0
Sushentsev Denis <sushentsevds@gmail.com>
2017-03-15 22:50:04 +0300

    Finished without tests

---
+++ semester_2/hw1/task_1/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Calculating the factorial
+        /// </summary>
+        /// <returns>The factorial of a number</returns>
+        private static int Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result: {0}", Factorial(n));
+        }
+    }
+}
