6b28f0d1d169371df87773e59e159e16a79cb395
Sushentsev Denis <sushentsevds@gmail.com>
2017-03-19 01:33:21 +0300

    Task 1.2 (#26)
    * Finished without tests
    
    * Finished
    
    * Finished, class renamed
    
    * Fibonacci counting using a linear algorithm, test method was divided into several tests

---
+++ semester_2/hw1/task_2/task_2.tests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace task_2.tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void FibonacciOfNegativeShouldBeZeroTest()
+        {
+            Assert.AreEqual(0, Program.Fibonacci(-100));
+            Assert.AreEqual(0, Program.Fibonacci(-1));
+        }
+
+        [TestMethod]
+        public void FibonacciOfZeroShouldBeOneTest()
+        {
+            Assert.AreEqual(1, Program.Fibonacci(0));
+        }
+
+        [TestMethod]
+        public void FibonacciOfOneShouldBeOneTest()
+        {
+            Assert.AreEqual(1, Program.Fibonacci(1));
+        }
+
+        [TestMethod]
+        public void FibonacciOfTwoShouldBeTwoTest()
+        {
+            Assert.AreEqual(2, Program.Fibonacci(2));
+        }
+
+        [TestMethod]
+        public void FibonacciOfTenShouldBeEightyNineTest()
+        {
+            Assert.AreEqual(89, Program.Fibonacci(10));
+        }
+    }
+}
---
+++ semester_2/hw1/task_2/task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task_2
+{
+    public class Program
+    {
+        /// <summary>
+        /// Calculating the n-th Fibonacci number
+        /// </summary>
+        /// <returns>Fibonacci number</returns>
+        public static int Fibonacci(int n)
+        {
+            if (n < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int previousNumber1 = 1;
+                int previousNumber2 = 1;
+                int currentNumber = 1;
+                for (int i = 2; i <= n; ++i)
+                {
+                    currentNumber = previousNumber1 + previousNumber2;
+                    previousNumber1 = previousNumber2;
+                    previousNumber2 = currentNumber;
+                }
+                return currentNumber;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The {n}th number is {Fibonacci(n)}");
+        }
+    }
+}
