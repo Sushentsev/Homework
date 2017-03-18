959230bda9e3840ec2764927a014c506fc10a99d
Sushentsev Denis <sushentsevds@gmail.com>
2017-03-19 01:35:18 +0300

    Task 1.3 (#27)
    * Finished without tests
    
    * Finished

---
+++ semester_2/hw1/task_3/task_3.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("task_3.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task_3.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5db94ba3-fa1e-46b0-909f-1519bdbd9451")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ semester_2/hw1/task_3/task_3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace task_3
+{
+    public class Program
+    {
+        /// <summary>
+        /// Bubble sorting
+        /// </summary>
+        /// <returns>sorted array</returns>
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; ++i)
+                for (int j = 0; j < array.Length - i - 1; ++j)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+        }
+
+        /// <summary>
+        /// Printing array
+        /// </summary>
+        private static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+                Console.Write("{0} ", array[i]);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Filling an array by random numbers from 1 to 10
+        /// </summary>
+        private static void InputArray(int[] array)
+        {
+            Random ran = new Random();
+            for (int i = 0; i < array.Length; ++i)
+                array[i] = ran.Next(1, 11);
+        }
+
+        static void Main(string[] args)
+        {
+            const int length = 10;
+            int[] array = new int[length];
+
+            InputArray(array);
+            Console.WriteLine("Initial array:");
+            PrintArray(array);
+            Console.WriteLine("Sorted array:");
+            BubbleSort(array);
+            PrintArray(array);
+        }
+    }
+}
