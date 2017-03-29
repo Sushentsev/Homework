23e5d946c6cdb82c929eae1662c9975972e88818
Sushentsev Denis <sushentsevds@gmail.com>
2017-03-29 23:16:13 +0300

    Class Stack

---
+++ semester_2/hw2/Task_1/Task_1.Tests/StackTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task_1.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            stack = new Stack();
+        }
+
+        /// <summary>
+        /// Push test with one element
+        /// </summary>
+        [TestMethod]
+        public void PushTest1()
+        {
+            stack.Push(2);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        /// <summary>
+        /// Push test with two elements
+        /// </summary>
+        [TestMethod]
+        public void PushTest2()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        /// <summary>
+        /// Pop test with one element
+        /// </summary>
+        [TestMethod]
+        public void PopTest1()
+        {
+            stack.Push(1);
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        /// <summary>
+        /// Pop test with two elements
+        /// </summary>
+        [TestMethod]
+        public void PopTest2()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+        }
+
+        /// <summary>
+        /// Pop test with empty stack
+        /// </summary>
+        [TestMethod]
+        public void PopTest3()
+        {
+            try
+            {
+                stack.Pop();
+            }
+            catch(Exception)
+            {
+
+            };
+        }
+
+        /// <summary>
+        /// Pop test with three elements
+        /// </summary>
+        [TestMethod]
+        public void LengthTest1()
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(3, stack.GetLength());
+        }
+
+        private Stack stack;
+    }
+}
---
+++ semester_2/hw2/Task_1/Task_1/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class Stack
+    {
+        /// <summary>
+        /// The element of stack
+        /// </summary>
+        private class StackElement
+        {
+            public StackElement Next;
+            public int Value;
+
+            public StackElement(StackElement next, int value)
+            {
+                Next = next;
+                Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Stack length
+        /// </summary>
+        private int length;
+
+        /// <summary>
+        /// The first element of stack
+        /// </summary>
+        private StackElement head = null;
+
+        /// <summary>
+        /// Pushing new value to stack
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            var newElement = new StackElement(head, value);
+            head = newElement;
+            ++length;
+        }
+
+        /// <summary>
+        /// Removing element from stack
+        /// </summary>
+        /// <returns></returns>
+        public int Pop()
+        {
+            if (length == 0)
+            {
+                throw new Exception("Check is empty!");
+            }
+            var temp = head.Value;
+            head = head.Next;
+            --length;
+            return temp;
+        }
+
+        /// <summary>
+        /// Checking stack on emptiness
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty() => length == 0;
+
+        /// <summary>
+        /// Getting the length of stack
+        /// </summary>
+        /// <returns></returns>
+        public int GetLength() => length;
+    }
+
+}
