c422894c6342688ce55c5804b6ed77d962e20dee
Sushentsev Denis <sushentsevds@gmail.com>
2017-04-08 22:57:55 +0300

    Functions Map, Filter and Fold

---
+++ semester_2/hw5/Task_1/Task_1.Tests/FunctionsTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task_1.Tests
+{
+    [TestClass]
+    public class FunctionsTests
+    {
+        private List<int> list;
+        private List<int> returningList;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int> { 1, 2, 3 };
+        }
+
+        [TestMethod]
+        public void MapTest1()
+        {
+            returningList = Functions.Map(list, x => x * 2);
+
+            for (var i = 0; i <= 2; ++i)
+            {
+                Assert.AreEqual(list[i] * 2, returningList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void MapTest2()
+        {
+            returningList = Functions.Map(list, x => x - 2);
+
+            for (var i = 0; i <= 2; ++i)
+            {
+                Assert.AreEqual(list[i] - 2, returningList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void FilterTest1()
+        {
+            returningList = Functions.Filter(list, x => (x % 2) == 0);
+            Assert.AreEqual(2, returningList[0]);
+        }
+
+        [TestMethod]
+        public void FilterTest2()
+        {
+            returningList = Functions.Filter(list, x => x != 2);
+            Assert.AreEqual(1, returningList[0]);
+            Assert.AreEqual(3, returningList[1]);
+        }
+
+        [TestMethod]
+        public void FoldTest1()
+        {
+            var value = Functions.Fold(list, 1, (acc, elem) => acc * elem);
+            Assert.AreEqual(6, value);
+        }
+
+        [TestMethod]
+        public void FoldTest2()
+        {
+            var value = Functions.Fold(list, 4, (acc, elem) => acc + elem);
+            Assert.AreEqual(10, value);
+        }
+    }
+}
---
+++ semester_2/hw5/Task_1/Task_1/Functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Функции Map, Filter, Fold
+    /// </summary>
+    public static class Functions
+    {
+        /// <summary>
+        /// Функция Map
+        /// </summary>
+        /// <param name="list">Связный список</param>
+        /// <param name="function">Функция</param>
+        /// <returns>Связный список, получаемый применением функции к каждому элементу</returns>
+        public static List<int> Map(List<int> list, Func<int, int> function)
+        {
+            var newList = new List<int>();
+
+            foreach (var element in list)
+            {
+                newList.Add(function(element));
+            }
+
+            return newList;
+        }
+
+        /// <summary>
+        /// Функция Filter
+        /// </summary>
+        /// <param name="list">Связный список</param>
+        /// <param name="function">Функция</param>
+        /// <returns>Связный список, составленный из элементов переданного списка, для которых переданная функция вернула true</returns>
+        public static List<int> Filter(List<int> list, Func<int, bool> function) => list.FindAll(element => function(element));
+
+        /// <summary>
+        /// Функция Fold
+        /// </summary>
+        /// <param name="list">Связный список</param>
+        /// <param name="value">Начальное значение</param>
+        /// <param name="function">Функция</param>
+        /// <returns>Накопленное значение, получившееся после всего прохода списка</returns>
+        public static int Fold(List<int> list, int acc, Func<int, int, int> function)
+        {
+            foreach (var element in list)
+            {
+                acc = function(acc, element);
+            }
+
+            return acc;
+        }
+    }
+}
