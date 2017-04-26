29931a2a68a4e37ee7754d11e2a29e2d398fd7a5
Sushentsev Denis <sushentsevds@gmail.com>
2017-04-27 01:46:23 +0300

    Cursor control based on class EventLoop

---
+++ semester_2/hw5/Task_2/Task_2/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Бизнес-логика игры
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Смещение курсора влева
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft > 0)
+            {
+                --Console.CursorLeft;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вправо
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnRight(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft < Console.BufferWidth - 1)
+            {
+                ++Console.CursorLeft;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вверх
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop > 0)
+            {
+                --Console.CursorTop;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вниз
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnDown(object sender, EventArgs args) => ++Console.CursorTop; 
+    }
+}
