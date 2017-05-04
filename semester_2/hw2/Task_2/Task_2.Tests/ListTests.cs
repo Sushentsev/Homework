0a7fd70ad68413e4bb610c2c6002f1dadf19bd3f
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-04 15:55:35 +0300

    Class List (#30)
    * Class List
    
    * Improved tests, comments
    
    * Minor changes
    
    * Corrected class List (not tested), added new exceptions
    
    * Tests updated

---
+++ semester_2/hw2/Task_2/Task_2.Tests/ListTests.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task_2.Tests
+{
+    [TestClass]
+    public class ListTests
+    {
+        private List list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List();
+        }
+
+        [TestMethod]
+        public void AddOneValueToCorrectPositionTest()
+        {
+            list.AddValueByPosition(5, 0);
+            Assert.AreEqual(5, list.GetValueByPosition(0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void AddOneValueToIncorrectPositionTest()
+        {
+            list.AddValueByPosition(5, 1);
+        }
+
+        [TestMethod]
+        public void AddTwoValuesToCorrectPositionsTest()
+        {
+            list.AddValueByPosition(5, 0);
+            list.AddValueByPosition(3, 0);
+            Assert.AreEqual(3, list.GetValueByPosition(0));
+            Assert.AreEqual(5, list.GetValueByPosition(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void AddTwoValuesToIncorrectPositionsTest()
+        {
+            list.AddValueByPosition(5, 0);
+            list.AddValueByPosition(5, -1);
+        }
+
+        [TestMethod]
+        public void AddSomeValuesToCorrectPositionsTest1()
+        {
+            for (var i = 0; i < 10; ++i)
+            {
+                list.AddValueByPosition(i, i);
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                Assert.AreEqual(i, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        public void AddSomeValuesToCorrectPositionsTest2()
+        {
+            for (var i = 0; i < 10; ++i)
+            {
+                list.AddValueByPosition(i, 0);
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                Assert.AreEqual(9 - i, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        public void EmptinessOfEmptyListTest()
+        {
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [TestMethod]
+        public void EmptinessOfNotEmptyListTest()
+        {
+            list.AddValueByPosition(5, 0);
+            Assert.IsFalse(list.IsEmpty());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void GetValueFromEmptyListTest()
+        {
+            list.GetValueByPosition(0);
+        }
+
+        [TestMethod]
+        public void GetValueByCorrectPositionFromNotEmptyListTest()
+        {
+            list.AddValueByPosition(10, 0);
+            Assert.AreEqual(10, list.GetValueByPosition(0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void GetValueByIncorrectPositionFromNotEmptyListTest()
+        {
+            list.AddValueByPosition(10, 0);
+            Assert.AreEqual(10, list.GetValueByPosition(-10));
+        }
+
+        [TestMethod]
+        public void GetValueByCorrectPositionFromListOfFiveElementsTest()
+        {
+            list.AddValueByPosition(3, 0);
+            list.AddValueByPosition(4, 1);
+            list.AddValueByPosition(2, 0);
+            list.AddValueByPosition(0, 0);
+            list.AddValueByPosition(1, 1);
+
+            for (var i = 0; i < 5; ++i)
+            {
+                Assert.AreEqual(i, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void GetValueByIncorrectPositionFromListOfFiveElementsTest()
+        {
+            list.AddValueByPosition(3, 0);
+            list.AddValueByPosition(4, 1);
+            list.AddValueByPosition(2, 0);
+            list.AddValueByPosition(0, 0);
+            list.AddValueByPosition(1, 1);
+
+            for (var i = 0; i < 8; ++i)
+            {
+                Assert.AreEqual(i, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void ChangeValueInEmptyList()
+        {
+            list.ChangeValueByPosition(9, 0);
+        }
+
+        [TestMethod]
+        public void ChangeValueByCorrectPositionInNotEmptyTest()
+        {
+            list.AddValueByPosition(0, 0);
+            list.ChangeValueByPosition(9, 0);
+            Assert.AreEqual(9, list.GetValueByPosition(0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void ChangeValueByIncorrectPositionInNotEmptyTest()
+        {
+            list.AddValueByPosition(0, 0);
+            list.ChangeValueByPosition(9, -2);
+        }
+
+        [TestMethod]
+        public void ChangeValuesByCorrectPositionsInListOfSomeElementsTest()
+        {
+            list.AddValueByPosition(3, 0);
+            list.AddValueByPosition(4, 1);
+            list.AddValueByPosition(2, 0);
+            list.AddValueByPosition(0, 0);
+            list.AddValueByPosition(1, 1);
+
+            for (var i = 0; i < 5; ++i)
+            {
+                list.ChangeValueByPosition(i + 5, i);
+                Assert.AreEqual(i + 5, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void RemoveFromEmptyListTest()
+        {
+            list.RemoveElementByPosition(0);
+        }
+
+        [TestMethod]
+        public void RemoveByCorrectPositionFromNotEmptyListTest()
+        {
+            list.AddValueByPosition(5, 0);
+            list.RemoveElementByPosition(0);
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OutOfIndexException))]
+        public void RemoveByIncorrectPositionFromNotEmptyListTest()
+        {
+            list.AddValueByPosition(5, 0);
+            list.RemoveElementByPosition(-1);
+        }
+
+        [TestMethod]
+        public void RemoveByCorrectPositionFromListOfFiveElementsTest()
+        {
+            list.AddValueByPosition(0, 0);
+            list.AddValueByPosition(1, 1);
+            list.AddValueByPosition(2, 2);
+            list.AddValueByPosition(3, 3);
+            list.AddValueByPosition(4, 4);
+
+            list.RemoveElementByPosition(1);
+            list.RemoveElementByPosition(2);
+
+            for (var i = 0; i < 3; ++i)
+            {
+                Assert.AreEqual(i * 2, list.GetValueByPosition(i));
+            }
+        }
+    }
+}
---
+++ semester_2/hw2/Task_2/Task_2/List.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Класс связный список
+    /// </summary>
+    public class List
+    {
+        /// <summary>
+        /// Элемент связного списка
+        /// </summary>
+        private class ListElement
+        {
+            /// <summary>
+            /// Указатель на следующий элемент списка
+            /// </summary>
+            public ListElement Next { get; set; }
+
+            /// <summary>
+            /// Значение элемента списка
+            /// </summary>
+            public int Value { get; set; }
+
+            /// <summary>
+            /// Конструктор для класса ListElement
+            /// </summary>
+            /// <param name="next">Указатель на следующий элемент</param>
+            /// <param name="value">Значение элемента</param>
+            public ListElement(ListElement next, int value)
+            {
+                this.Next = next;
+                this.Value = value;
+            }
+
+        }
+
+        /// <summary>
+        /// Указатель на первый элемент списка
+        /// </summary>
+        private ListElement head;
+
+        /// <summary>
+        /// Длина списка
+        /// </summary>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Добавление нового элемента в нужную позицию.
+        /// Нумерация начинается с 0
+        /// </summary>
+        /// <param name="value">Значение для добавления</param>
+        /// <param name="position">Позиция для добавления</param>
+        public void AddValueByPosition(int value, int position)
+        {
+            if (position < 0 || position > Length)
+            {
+                throw new OutOfIndexException("Incorrect position!");
+            }
+
+            if (position == 0)
+            {
+                var newHeadElement = new ListElement(head, value);
+                head = newHeadElement;
+                ++Length;
+                return;
+            }
+
+            var cursor = head;
+            for (var i = 0; i < position - 1; ++i)
+            {
+                cursor = cursor.Next;
+            }
+            var newElement = new ListElement(cursor.Next, value);
+            cursor.Next = newElement;
+            ++Length;
+        }
+
+        /// <summary>
+        /// Проверка списка на пустоту
+        /// </summary>
+        /// <returns>True, если список пуст, иначе false </returns>
+        public bool IsEmpty() => Length == 0;
+
+        /// <summary>
+        /// Получение значения элемента по его позиции
+        /// </summary>
+        /// <param name="position">Необходимая позиция</param>
+        /// <returns>Значение элемента</returns>
+        public int GetValueByPosition(int position)
+        {
+            if (position < 0 || position >= Length)
+            {
+                throw new OutOfIndexException("Incorrect position!");
+            }
+
+            var cursor = head;
+            for (var i = 0; i < position; ++i)
+            {
+                cursor = cursor.Next;
+            }
+
+            return cursor.Value;
+        }
+
+        /// <summary>
+        /// Изменения значения элемента по его позиции
+        /// </summary>
+        /// <param name="value">Новое значение</param>
+        /// <param name="position">Необходимая позиция</param>
+        public void ChangeValueByPosition(int value, int position)
+        {
+            if (position < 0 || position >= Length)
+            {
+                throw new OutOfIndexException("Incorrect position!");
+            }
+
+            var cursor = head;
+            for (var i = 0; i < position; ++i)
+            {
+                cursor = cursor.Next;
+            }
+            cursor.Value = value;
+        }
+
+        /// <summary>
+        /// Удаление элемента в заданной позиции
+        /// </summary>
+        /// <param name="position">Позиция для удаления</param>
+        public void RemoveElementByPosition(int position)
+        {
+            if (position < 0 || position >= Length)
+            {
+                throw new OutOfIndexException("Incorrect position!");
+            }
+
+            if (position == 0)
+            {
+                head = head.Next;
+                --Length;
+                return;
+            }
+
+            var cursor = head;
+            for (var i = 0; i < position - 1; ++i)
+            {
+                cursor = cursor.Next;
+            }
+            cursor.Next = cursor.Next.Next;
+            --Length;
+        }  
+    }
+}
