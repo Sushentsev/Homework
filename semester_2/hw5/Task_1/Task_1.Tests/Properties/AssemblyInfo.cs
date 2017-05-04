fe3d18d62eded8ad022e3bc3caa53b55b858c8ef
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-04 16:02:08 +0300

    Functions Map, Filter and Fold (#36)
    * Functions Map, Filter and Fold
    
    * Corrected function Filter

---
+++ semester_2/hw5/Task_1/Task_1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task_1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19c876d7-0ab8-496d-ab30-cc2fc3151314")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
