652a6e625afa0c6479459ee6fc75594907ef59c4
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-04 15:57:22 +0300

    Class Stack (#31)
    * Class Stack
    
    * Improved tests, comments
    
    * Minor changes

---
+++ semester_2/hw2/Task_1/Task_1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task_1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("faa272e0-f6a5-4c4b-92e9-6ef53bcce0a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
