6f083245c79b2120e92acb259cefdb43ec17752f
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-04 15:58:35 +0300

    Class HashTable with a choice of hash function (#34)
    * Class HashTable with a choice of hash function
    
    * Minor changes

---
+++ semester_2/hw3/Task_2/Task_2.Tests/HashTableTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task_2.Tests
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        private HashTable table;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            table = new HashTable(new HashFunction1());
+        }
+
+        [TestMethod]
+        public void IsContainedInEmptyTableTest()
+        {
+            Assert.IsFalse(table.IsContained("dfd"));
+        }
+
+        [TestMethod]
+        public void IsContainedInNotEmptyTableTest1()
+        {
+            table.Add("first");
+            table.Add("second");
+            table.Add("third");
+            Assert.IsTrue(table.IsContained("first"));
+        }
+
+        [TestMethod]
+        public void IsContainedInNotEmptyTableTest2()
+        {
+            table.Add("first");
+            table.Add("second");
+            table.Add("third");
+            Assert.IsTrue(table.IsContained("third"));
+        }
+
+        [TestMethod]
+        public void IsContainedInNotEmptyTableTest3()
+        {
+            table.Add("first");
+            table.Add("second");
+            table.Add("third");
+            Assert.IsFalse(table.IsContained("bored"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void RemoveFromEmptyTableTest()
+        {
+            table.Remove("memchik");
+        }
+
+        [TestMethod]
+        public void RemoveFromNotEmptyTableTest1()
+        {
+            table.Add("first");
+            table.Add("second");
+            table.Add("third");
+            table.Remove("second");
+            Assert.IsFalse(table.IsContained("second"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void RemoveFromNotEmptyTableTest2()
+        {
+            table.Add("first");
+            table.Add("second");
+            table.Add("third");
+            table.Remove("drink");
+        }
+    }
+}
+
---
+++ semester_2/hw3/Task_2/Task_2/HashFunction2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Function for hashing
+    /// </summary>
+    public class HashFunction2 : IHashFunction
+    {
+        /// <summary>
+        /// Getting hash value
+        /// </summary>
+        /// <param name="word">Word for hashing</param>
+        /// <returns>Hash value</returns>
+        public uint GetHash(string word)
+        {
+            uint hash = 0;
+
+            foreach (var symbol in word)
+            {
+                hash = hash * 1664525 + symbol + 1013904223;
+            }
+
+            return hash;
+        }
+    }
+}
