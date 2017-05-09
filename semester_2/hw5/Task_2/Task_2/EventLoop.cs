8970f2638c1b81e0c5ea1535aaeaa1a4abefe808
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-09 13:14:05 +0300

    Cursor control based on class EventLoop (#38)
    * Cursor control based on class EventLoop
    
    * Fixed some bugs

---
+++ semester_2/hw5/Task_2/Task_2/EventLoop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Класс EventLoop для обработки событий
+    /// </summary>
+    public class EventLoop
+    {
+        /// <summary>
+        /// Событие при нажатии стрелки влево
+        /// </summary>
+        public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Событие при нажатии стрелки вправо
+        /// </summary>
+        public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Событие при нажатии стрелки вверх
+        /// </summary>
+        public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Событие при нажатии стрелки вниз
+        /// </summary>
+        public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+        /// <summary>
+        /// Обработка нажатий на стрелки
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        LeftHandler(this, EventArgs.Empty);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        RightHandler(this, EventArgs.Empty);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        UpHandler(this, EventArgs.Empty);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        DownHandler(this, EventArgs.Empty);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ semester_2/hw5/Task_2/Task_2/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Бизнес-логика игры
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Смещение курсора влева
+        /// </summary>
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft > 0)
+            {
+                --Console.CursorLeft;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вправо
+        /// </summary>
+        public void OnRight(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft < Console.BufferWidth - 1)
+            {
+                ++Console.CursorLeft;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вверх
+        /// </summary>
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop > 0)
+            {
+                --Console.CursorTop;
+            }
+        }
+
+        /// <summary>
+        /// Смещение курсора вниз
+        /// </summary>
+        public void OnDown(object sender, EventArgs args)
+        {
+            if (Console.CursorTop < Console.BufferHeight - 1)
+            {
+                ++Console.CursorTop;
+            }
+        }
+    }
+}
