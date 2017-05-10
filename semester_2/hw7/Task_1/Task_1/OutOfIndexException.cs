1559ad0053d9139697e0a698ed0a77c6f1f9250b
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-10 22:19:49 +0300

    Generic Stack and List (#39)

---
+++ semester_2/hw7/Task_1/Task_1/OutOfIndexException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Исключение, которое бросается, если индекс позиции не принадлжежит доупстимым значениям
+    /// </summary>
+    [Serializable]
+    public class OutOfIndexException : Exception
+    {
+        public OutOfIndexException() { }
+        public OutOfIndexException(string message) : base(message) { }
+        public OutOfIndexException(string message, Exception inner) : base(message, inner) { }
+        protected OutOfIndexException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
