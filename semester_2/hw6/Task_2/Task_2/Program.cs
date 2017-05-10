a3546b81efe9ab2bb9e6055834e80528072c95e5
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-10 22:18:21 +0300

    Corrected style guide

--- semester_2/hw6/Task_2/Task_2/Program.cs
+++ semester_2/hw6/Task_2/Task_2/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task_2
@@ -16,7 +13,7 @@ namespace Task_2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new clockForm());
+            Application.Run(new ClockForm());
         }
     }
 }
