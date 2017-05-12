fc3379dacf29097aeb18adbed76241def6e0439b
Sushentsev Denis <sushentsevds@gmail.com>
2017-05-12 10:44:33 +0300

    Clock (#37)
    * Clock
    
    * Corrected style guide
    
    * Added class Clock

---
+++ semester_2/hw6/Task_2/Task_2/Clock.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Task_2
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClockForm));
+            this.time = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time.Location = new System.Drawing.Point(12, 9);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(212, 55);
+            this.time.TabIndex = 0;
+            this.time.Text = "00:00:00";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 73);
+            this.Controls.Add(this.time);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ClockForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Часы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ semester_2/hw6/Task_2/Task_2/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    /// <summary>
+    /// Приложение, показывающее текущее время
+    /// </summary>
+    public partial class ClockForm : Form
+    {
+        /// <summary>
+        /// Конструктор для класса ClockForm
+        /// </summary>
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Событие, возникающее при тиканье таймера
+        /// </summary>
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            time.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
