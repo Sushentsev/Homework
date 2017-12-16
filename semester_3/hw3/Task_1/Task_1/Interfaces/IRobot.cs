85ccc6864b2e86430552f1003eeec02a6e7e37b0
Denis <sushentsevds@gmail.com>
2017-12-16 15:44:25 +0300

    Added interfaces.

---
+++ semester_3/hw3/Task_1/Task_1/Interfaces/IRobot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Task_1.Interfaces
+{
+    /// <summary>
+    /// Interface for robot.
+    /// </summary>
+    interface IRobot
+    {
+        /// <summary>
+        /// Started node for robot.
+        /// </summary>
+        int StartNode { get; }
+
+        /// <summary>
+        /// List of nodes which robot can visit.
+        /// </summary>
+        List<int> VisitedNodes { get; }
+
+        /// <summary>
+        /// Set graph to robot.
+        /// </summary>
+        /// <param name="graph">Adjacency matrix.</param>
+        void SetGraph(int[,] graph);
+    }
+}
---
+++ semester_3/hw3/Task_1/Task_1/Interfaces/IRobotsMaker.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_1.Interfaces
+{
+    /// <summary>
+    /// Interface for maker.
+    /// </summary>
+    interface IRobotsMaker
+    {
+        /// <summary>
+        /// Is there a sequence in which all the robots will be destroyed.
+        /// </summary>
+        /// <returns>True — exist, false — otherwise.</returns>
+        bool IsSequenceExists();
+    }
+}
