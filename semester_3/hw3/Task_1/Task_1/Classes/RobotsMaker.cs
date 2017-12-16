d2852e33686a327b07123df526f49d52a6c23b37
Denis <sushentsevds@gmail.com>
2017-12-16 18:08:50 +0300

    Finished but not tested

--- semester_3/hw3/Task_1/Task_1/Classes/RobotsMaker.cs
+++ semester_3/hw3/Task_1/Task_1/Classes/RobotsMaker.cs
@@ -1,9 +1,13 @@
-﻿using Task_1.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Task_1.Interfaces;
 
 namespace Task_1.Classes
 {
     /// <summary>
-    /// Class robotsMaker.
+    /// Class RobotsMaker.
     /// </summary>
     public class RobotsMaker : IRobotsMaker
     {
@@ -23,7 +27,48 @@ namespace Task_1.Classes
         /// <param name="filepath"></param>
         public RobotsMaker(string filepath)
         {
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader(filepath))
+                {
+                    // Loading number of nodes.
+                    var line = string.Empty;
+                    string[] bits = line.Split(' ');
+                    var numberOfNodes = int.Parse(sr.ReadLine());
+                    graph = new bool[numberOfNodes, numberOfNodes];
 
+                    // Loading adjacency matrix.
+                    for (var i = 0; i < numberOfNodes; ++i)
+                    {
+                        line = sr.ReadLine();
+                        bits = line.Split(' ');
+                        for (var j = 0; j < numberOfNodes; ++j)
+                        {
+                            graph[i, j] = int.Parse(bits[j]) == 1 ? true : false;
+                        }
+                    }
+
+                    //Loading number of robots, starting nodes.
+                    var numberOfRobots = int.Parse(sr.ReadLine());
+                    robots = new Robot[numberOfRobots];
+                    line = sr.ReadLine();
+                    bits = line.Split(' ');
+
+                    // Initializing array of robots.
+                    for (var i = 0; i < numberOfRobots; ++i)
+                    {
+                        robots[i] = new Robot(int.Parse(bits[i]), graph);
+                    }
+
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
 
         /// <summary>
@@ -45,8 +90,31 @@ namespace Task_1.Classes
             }
         }
 
+        /// <summary>
+        /// Is there a sequence in which all the robots will be destroyed.
+        /// </summary>
+        /// <returns>True — exist, false — otherwise.</returns>
         public bool IsSequenceExists()
         {
+            var numberOfRobots = robots.Length;
+            var intersection = robots[0].TeleportingNodes;
+
+            if (numberOfRobots == 1)
+            {
+                return false;
+            }
+
+            // Finding intersection of teleporting nodes of all robots.
+            foreach (var robot in robots)
+            {
+                intersection.Intersect(robot.TeleportingNodes);
+            }
+
+            if (intersection.Count == 0)
+            {
+                return false;
+            }
+
             return true;
         }
     }
