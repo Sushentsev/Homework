71cd10984c088dd61cc4e1c4cd4289ec2964eb94
Denis <sushentsevds@gmail.com>
2018-02-24 14:46:38 +0300

    Improved task1 and task4

--- semester_4/hw1/Task_1-4/Task_1-4/Program.fs
+++ semester_4/hw1/Task_1-4/Task_1-4/Program.fs
@@ -1,5 +1,8 @@
 ﻿let rec factorial n =
-    if n = 0 then 1 else n * factorial (n - 1)
+    match n with
+    | n when n < 0 -> -1
+    | 0 -> 1
+    | _ -> n * factorial (n - 1)
 
 let fibonacci n =
     [1..n] |> List.fold (fun (l, r) _ -> (r, l + r)) (0, 1) |> fst
@@ -11,7 +14,10 @@ let reverse ls =
 
 let makeList n m =
     let rec pow n =
-        if n = 0 then 1 else 2 * pow (n - 1)
+        match n with
+        | n when n < 0 -> -1
+        | 0 -> 1
+        | _ -> 2 * pow (n - 1)
     let rec make m acc = 
         if m < 0 then [] else acc :: make (m - 1) (acc * 2)
     make m (pow n)
