8b58fd0a256739ce7d5b7fc049205af453a28466
Denis <sushentsevds@gmail.com>
2018-03-04 15:06:02 +0300

    Improved task1, added task4

--- semester_4/hw2/Task_1-4/Task_1-4/Program.fs
+++ semester_4/hw2/Task_1-4/Task_1-4/Program.fs
@@ -1,5 +1,7 @@
-﻿let rec product n = 
-    if abs(n) < 10 then abs(n) else (abs(n) % 10) * product (n / 10)
+﻿let product n = 
+    let rec productPositiveValue value = 
+        if value < 10 then value else (value % 10) * productPositiveValue (value / 10)
+    productPositiveValue (abs(n))
 
 let findPosition ls n =
     let rec position ls pos =
@@ -11,3 +13,21 @@ let findPosition ls n =
 let isPalindrome str =
     let newStr = str |> Seq.filter (fun x -> x <> ' ') |> Seq.toList
     newStr = List.rev newStr
+
+let rec mergeSort ls =
+    let rec split ls (left, right) =
+        match ls with
+        | [] -> (left, right)
+        | [a] -> (a :: left, right)
+        | a :: b :: tail -> split tail (a :: left, b :: right)
+    let rec merge (left, right) = 
+        match (left, right) with
+        | (left, []) -> left
+        | ([], right) -> right
+        | (hl :: tl, hr :: tr) -> if hl < hr then hl :: merge (tl, right) else hr :: merge (left, tr)
+    match ls with
+    | [] -> ls
+    | [a] -> ls
+    | _ -> 
+        let left, right = split ls ([], [])
+        merge (mergeSort left, mergeSort right)
