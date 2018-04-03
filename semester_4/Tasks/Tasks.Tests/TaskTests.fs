99dc3d779d486ad3cd24b20d53ad1851c4d76a4c
Denis <sushentsevds@gmail.com>
2018-04-03 11:16:31 +0300

    Done

---
+++ semester_4/Tasks/Tasks.Tests/TaskTests.fs
@@ -0,0 +1,37 @@
+﻿open FsUnit
+open NUnit.Framework
+open MyTasks.Task1
+open MyTasks.Task2
+open System
+
+
+module Task1_Tests = 
+    
+    [<Test>]
+    let ``List of zeros`` = 
+        [0.0; 0.0; 0.0] |> averageSin |> should equal 0.0
+    
+    [<Test>]
+    let ``List of pi/2`` = 
+        [(Math.PI) / 2.0; (Math.PI) / 2.0; (Math.PI) / 2.0] |> averageSin |> should equal 1.0
+
+module Task2_Tests = 
+
+    [<Test>]
+    let ``Empty tree`` () = 
+        Empty |> getMinHeight |> should equal 0
+    
+    [<Test>]
+    let ``Not empty tree1`` () = 
+        let tree = Tree(5, Empty, Empty)
+        tree |> getMinHeight |> should equal 0
+
+    [<Test>]
+    let ``Not empty tree2`` () = 
+        let tree = Tree(5, Empty, Tree(6, Empty, Empty))
+        tree |> getMinHeight |> should equal 0
+
+    [<Test>]
+    let ``Not empty tree3`` () = 
+        let tree = Tree(5, Tree(6, Empty, Empty), Tree(7, Empty, Empty))
+        tree |> getMinHeight |> should equal 1
---
+++ semester_4/Tasks/Tasks/Program.fs
@@ -0,0 +1,27 @@
+﻿namespace MyTasks
+
+module Task1 = 
+
+    let averageSin ls = 
+        let rec sumSin ls (acc : float) =
+            match ls with
+            | [] -> acc
+            | head :: tail -> sumSin tail (acc + sin(head))
+        (sumSin ls 0.0) / (float)ls.Length
+
+module Task2 = 
+    
+    type Tree<'a> = 
+        | Node of 'a * Tree<'a> * Tree<'a>
+        | Empty
+
+    let getMinHeight tree = 
+        let rec getMinHeight' tree ls acc = 
+            match tree with
+            | Node(a, left, Empty) -> acc :: ls
+            | Node(a, Empty, right) -> acc :: ls
+            | Node(a, left, right) -> 
+                getMinHeight' left ls (acc + 1) |> ignore
+                getMinHeight' right ls (acc + 1)
+            | _ -> ls
+        getMinHeight' tree [] 0
