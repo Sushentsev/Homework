1dd2042735a9631455825dd9fb9180c78fd131c1
Denis <sushentsevds@gmail.com>
2018-04-07 23:54:02 +0300

    Implemented Task 1 with tests

---
+++ semester_4/hw5/Task_1-3/Task_1-3.Tests/TasksTests.fs
@@ -0,0 +1,66 @@
+﻿open NUnit.Framework
+open FsUnit
+open Homework5.Task1
+
+
+module Task1_Tests = 
+    
+    [<Test>]
+    let ``Empty string test should be true`` () = 
+        let str = ""
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``String without brackets test should be true`` () = 
+        let str = "qwerty42"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Correct test1`` () = 
+        let str = "efw()wd(we(we))"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Correct test2`` () = 
+        let str = "[[adsddd[a]sdsd]d]"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Correct test3`` () = 
+        let str = "{}{}asa{aa}{{}a}"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Correct test4`` () = 
+        let str = "([{adadd{(ad)}ad}]adad)dd"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Correct test5`` () = 
+        let str = "{}sfsf[]add(a){q(as)q}"
+        str |> isBalanced |> should equal true
+
+    [<Test>]
+    let ``Incorrect test1`` () = 
+        let str = "eerer)(wdw"
+        str |> isBalanced |> should equal false
+
+    [<Test>]
+    let ``Incorrect test2`` () = 
+        let str = "wdwd[wef]wr["
+        str |> isBalanced |> should equal false
+
+    [<Test>]
+    let ``Incorrect test3`` () = 
+        let str = "{{sdsdd}sfsf}}"
+        str |> isBalanced |> should equal false
+
+    [<Test>]
+    let ``Incorrect test4`` () = 
+        let str = "{}sfsf([)]sff{}"
+        str |> isBalanced |> should equal false
+
+    [<Test>]
+    let ``Incorrect test5`` () = 
+        let str = "{}sfsf[]add(aq(as)q}"
+        str |> isBalanced |> should equal false
---
+++ semester_4/hw5/Task_1-3/Task_1-3/Tasks.fs
@@ -0,0 +1,19 @@
+﻿namespace Homework5
+
+module Task1 = 
+   
+    let isBalanced str =
+        let pairs  = [('(', ')'); ('[', ']'); ('{', '}')]
+        let isPair (a, b) = pairs |> List.contains (a, b)
+        let rec isBalanced' (str, ls) =
+            match str with
+            | h :: t when (h = '(' || h = '[' || h = '{') -> isBalanced' (t, h :: ls)
+            | h :: t when (h = ')' || h = ']' || h = '}') -> match ls with
+                                                             | [] -> false
+                                                             | _ -> if isPair(ls.Head, h) then isBalanced' (t, ls.Tail) else false
+            | _ :: t -> isBalanced' (t, ls)
+            | [] -> ls.IsEmpty
+        isBalanced' (str |> Seq.toList, [])
+
+
+    
