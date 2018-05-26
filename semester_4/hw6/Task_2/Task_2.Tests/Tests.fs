2c4aa8300014230f0ee46553001d58e511840faa
Denis <sushentsevds@gmail.com>
2018-05-26 19:53:00 +0300

    Implemented binary tree with tests

---
+++ semester_4/hw6/Task_2/Task_2.Tests/Tests.fs
@@ -0,0 +1,81 @@
+﻿open NUnit.Framework
+open FsUnit
+open BinaryTree
+
+module Tests = 
+    
+    let mutable tree = new BinaryTree<int>()
+
+    //// <summary>
+    /// Sets up a tree.
+    /// </summary>
+    [<SetUp>]
+    let ``SetUp`` () = 
+        tree <- new BinaryTree<int>()
+        (tree :> IBinaryTree<int>).AddValue(6);
+        (tree :> IBinaryTree<int>).AddValue(3);
+        (tree :> IBinaryTree<int>).AddValue(8);
+        (tree :> IBinaryTree<int>).AddValue(2);
+        (tree :> IBinaryTree<int>).AddValue(4);
+        (tree :> IBinaryTree<int>).AddValue(1);
+        (tree :> IBinaryTree<int>).AddValue(5);
+        (tree :> IBinaryTree<int>).AddValue(7);
+        (tree :> IBinaryTree<int>).AddValue(10);
+        (tree :> IBinaryTree<int>).AddValue(9);
+
+    //// <summary>
+    /// Simple enumerator test.
+    /// </summary>
+    [<Test>]
+    let ``Enumerator test`` () =
+        let expected = [| for i in 1..10 -> i |]
+        let actual = [| for i in tree -> i |]
+        actual |> should equal expected
+    
+    //// <summary>
+    /// Simple IsContained test.
+    /// </summary>
+    [<Test>]
+    let ``IsContained test`` () =
+        for i in 1..10 do
+            (tree :> IBinaryTree<int>).IsContained i |> should equal true
+        (tree :> IBinaryTree<int>).GetLength |> should equal 10
+
+    //// <summary>
+    /// Simple remove existed values test.
+    /// </summary>
+    [<Test>]
+    let ``Simple remove existed values test`` () = 
+        (tree :> IBinaryTree<int>).RemoveValue 1
+        (tree :> IBinaryTree<int>).RemoveValue 10
+        (tree :> IBinaryTree<int>).IsContained 1 |> should equal false
+        (tree :> IBinaryTree<int>).IsContained 10 |> should equal false
+        for i in 2..9 do
+            (tree :> IBinaryTree<int>).IsContained i |> should equal true
+        (tree :> IBinaryTree<int>).GetLength  |> should equal 8
+        (tree :> IBinaryTree<int>).RemoveValue 7
+        (tree :> IBinaryTree<int>).IsContained 7 |> should equal false
+        (tree :> IBinaryTree<int>).GetLength  |> should equal 7
+      
+    //// <summary>
+    /// Complicated remove existed values test.
+    /// Should save the structure of a binary tree.
+    /// </summary>
+    [<Test>]
+    let ``Complicated remove existed values test`` () = 
+        (tree :> IBinaryTree<int>).RemoveValue 3
+        (tree :> IBinaryTree<int>).RemoveValue 6
+        (tree :> IBinaryTree<int>).RemoveValue 8
+        (tree :> IBinaryTree<int>).RemoveValue 4
+        let expected = [| 1; 2; 5; 7; 9; 10 |]
+        let actual = [| for i in tree -> i |]
+        actual |> should equal expected 
+    
+    //// <summary>
+    /// Simple remove not existed values test.
+    /// An exception should be thrown.
+    /// </summary>
+    [<Test>]
+    let ``Simple remove not existed values test`` () = 
+        (fun () -> (tree :> IBinaryTree<int>).RemoveValue 0 |> ignore)
+            |> should throw typeof<System.Exception>
---
+++ semester_4/hw6/Task_2/Task_2/BinaryTree.fs
@@ -0,0 +1,131 @@
+﻿module BinaryTree
+open System.Collections
+open System.Collections.Generic
+
+/// <summary>
+/// Type for binary tree node.
+/// </summary>
+type TreeNode<'T> = 
+    | TreeNode of 'T * TreeNode<'T> * TreeNode<'T>
+    | Empty
+
+/// <summary>
+/// Interface for binary tree node.
+/// </summary>
+type IBinaryTree<'T> =
+
+    /// <summary>
+    /// Adds new value to binary tree.
+    /// </summary>
+    abstract AddValue : 'T -> unit
+
+    /// <summary>
+    /// Removes value from tree.
+    /// If value is not found, exception will be thrown.
+    /// </summary>
+    abstract RemoveValue : 'T -> unit
+
+    /// <summary>
+    /// Checks whether the value is contained in the tree.
+    /// Returns true if the value is contained, otherwise false.
+    /// </summary>
+    abstract IsContained : 'T -> bool
+
+    /// <summary>
+    /// Gets the length of tree.
+    /// </summary>
+    abstract GetLength : int with get
+
+/// <summary>
+/// Binary tree class.
+/// Contains all methods for performing basic binary tree functions.
+/// </summary>
+/// <remarks>
+/// Implements <see cref="IBinaryTree{'T}"/> and <see cref="IEnumerable{'T}"/> interfaces.
+/// </remarks>
+type BinaryTree<'T when 'T : comparison>() =
+
+    let mutable root = Empty
+    let mutable length = 0
+    let ascendingSeq () = 
+        let rec ascendingSeq' (node : TreeNode<'T>) = 
+            seq {
+                match node with
+                | TreeNode(nodeValue, left, right) -> 
+                    yield! ascendingSeq' left
+                    yield nodeValue
+                    yield! ascendingSeq' right
+                | Empty -> yield! Seq.empty
+            }
+        ascendingSeq' root
+
+    interface IBinaryTree<'T> with
+
+        member this.AddValue (value : 'T) =
+
+            let rec addValue (value : 'T) (node : TreeNode<'T>) = 
+                match node with
+                | Empty -> TreeNode(value, Empty, Empty)     
+                | TreeNode(nodeValue, left, right) -> 
+                    match value with
+                    | value when value < nodeValue -> TreeNode(nodeValue, addValue value left, right)
+                    | value when value > nodeValue -> TreeNode(nodeValue, left, addValue value right)
+                    | _ -> failwith "The value is already contained in the tree!"
+
+            root <- addValue value root
+            length <- length + 1
+
+        member this.RemoveValue (value : 'T) = 
+
+            let rec getMinNode (node : TreeNode<'T>) = 
+                match node with
+                | Empty -> failwith "The node is empty!"
+                | TreeNode(_, leftChild, _) -> 
+                    match leftChild with
+                    | Empty -> node
+                    | _ -> getMinNode leftChild
+
+            let rec removeValue (value : 'T) (node : TreeNode<'T>) = 
+                match node with 
+                | Empty -> failwith "The value is not found!"
+                | TreeNode(nodeValue, left, right) -> 
+                    match value with
+                    | value when value < nodeValue -> TreeNode(nodeValue, removeValue value left, right)
+                    | value when value > nodeValue -> TreeNode(nodeValue, left, removeValue value right)
+                    | _ -> 
+                        match left, right with
+                        | Empty, Empty -> Empty
+                        | Empty, TreeNode(_, _, _) -> right
+                        | TreeNode(_, _, _), Empty -> left
+                        | _ -> 
+                            let minNode = getMinNode(right)
+                            match minNode with
+                            | TreeNode(minNodeValue, _, _) -> TreeNode(minNodeValue, left, removeValue minNodeValue right)
+                            | Empty -> failwith "The node is empty!"
+          
+            root <- removeValue value root
+            length <- length - 1
+
+        member this.IsContained (value : 'T) =
+
+            let rec isContained (value : 'T) (node : TreeNode<'T>) = 
+                match node with
+                | Empty -> false
+                | TreeNode(nodeValue, left, right) -> 
+                    match value with
+                    | value when value < nodeValue -> isContained value left
+                    | value when value > nodeValue -> isContained value right
+                    | _ -> true
+
+            isContained value root
+
+        member this.GetLength
+            with get () = length
+            
+    interface IEnumerable<'T> with
+
+        member this.GetEnumerator(): IEnumerator<'T> =
+            ascendingSeq().GetEnumerator()
+
+        member this.GetEnumerator(): IEnumerator =
+            ascendingSeq().GetEnumerator() :> IEnumerator
